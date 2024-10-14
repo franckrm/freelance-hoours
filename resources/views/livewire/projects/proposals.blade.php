374451bf17dd928aa0979af2d89ebffa33898987
3º Sgt Ribeiro <franckrm2001@gmail.com>
2024-10-14 16:38:43 -0300

    feat: create livewire index,show and proposals components

---
+++ resources/views/livewire/projects/proposals.blade.php
@@ -0,0 +1,3 @@
+<div>
+    componenete livewire projects.proposals
+</div>
---
+++ resources/views/livewire/projects/show.blade.php
@@ -0,0 +1,9 @@
+<div>
+    Componente livewire: project.show
+
+    <pre>
+        title: {{$project->title}}
+        description: {!! $project->description !!}
+    </pre>
+    
+</div>
