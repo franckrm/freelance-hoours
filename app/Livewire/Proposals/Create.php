12b3b00a2443b3f542517d9e2d2687a57a45bb02
3º Sgt Ribeiro <franckrm2001@gmail.com>
2024-10-29 17:14:08 -0300

    feat: adiciona funcao para salvar propostas

---
+++ app/Livewire/Proposals/Create.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Livewire\Proposals;
+
+use App\Models\Project;
+use Livewire\Component;
+use Illuminate\Validation\Rule;
+
+class Create extends Component
+{
+    public Project $project;
+    public bool $modal = true;
+    public string $email = '';
+    public int $hours = 0;
+
+    public bool $agree = false;
+
+    protected function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'hours' => 'required|numeric|gt:0',
+        ];
+    }
+
+    public function save()
+    {
+        
+        $this->validate();
+
+        
+        if(!$this->agree){
+            $this->addError('agree', 'Você precisa concordar com os termos de uso');
+            return;
+        }
+
+        $this->project->proposals()->updateOrCreate(
+            ['email' => $this->email],
+            ['hours' => $this->hours]
+        );
+
+        $this->modal = false;
+    }
+
+    public function render()
+    {
+        return view('livewire.proposals.create');
+    }
+}
