507f0d2b84b3d7b0f01f9068311ce8457b8852ca
3º Sgt Ribeiro <franckrm2001@gmail.com>
2024-11-03 06:41:05 -0300

    feat: cria dispatch para avisar o componente proposals que uma nova proposta foi criada

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -1,16 +1,40 @@
 <?php
 
 namespace App\Livewire\Projects;
-
+use Livewire\Attributes\Computed;
 use App\Models\Project;
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 class Proposals extends Component
 {
 
     public Project $project;
 
+    public int $qty = 5;
 
+    #[Computed()]
+    public function proposals()
+    {
+        return $this->project->proposals()
+                ->orderBy('hours')
+                ->paginate($this->qty);
+
+    }
+
+
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()->latest()->first()->created_at->diffForHumans();
+    }
+
+    public function loadMore()
+    { 
+        $this->qty += 5;
+    }
+
+    #[On('proposal::created')]
     public function render()
     {
         return view('livewire.projects.proposals');
